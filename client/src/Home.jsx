a547efb780003c9c87c46ef07cc2ee2c929b8cc7
Fakhri Athallah <fakhriwongsolo@gmail.com>
2024-05-18 08:39:09 +0700

    feat: added section LayananKami and BantuanHukum

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -4,6 +4,10 @@ import logo from './images/LegalAssist-White.png';
 import profile from './images/profile-icon.png';
 import notification from './images/notification-icon.png';
 import hero from './images/hero-img.png';
+import feature1 from './images/feature-1.png';
+import feature2 from './images/feature-2.png';
+import feature3 from './images/feature-3.png';
+import profile1 from './images/profile-pict1.png';
 
 function home() {
   return (
@@ -36,6 +40,67 @@ function home() {
         </div>
         <div className=''>
           <img src={hero} alt='' />
+        </div>
+      </div>
+      {/* Layanan Kami */}
+      <div>
+        <div>
+          <h1>Layanan Kami</h1>
+        </div>
+        <div className='flex justify-between items-start'>
+          <div className='w-[288px]'>
+            <img src={feature1} alt='' />
+            <h1 className='text-[32px]'>Info Hukum</h1>
+            <p className='text-[16px]'>
+              Dalam dunia yang kompleks ini, pengetahuan adalah kekuatan. Kami
+              memahami bahwa setiap keputusan hukum Anda membutuhkan pemahaman
+              yang komprehensif dan terperinci. Itulah sebabnya kami hadir,
+              sebagai sumber informasi hukum yang tepercaya dan terkini.
+            </p>
+          </div>
+          <div className='w-[288px]'>
+            <img src={feature2} alt='' />
+            <h1 className='text-[32px]'>Tips Hukum</h1>
+            <p className='text-[16px]'>
+              Dalam dunia yang kompleks ini, pengetahuan adalah kekuatan. Kami
+              memahami bahwa setiap keputusan hukum Anda membutuhkan pemahaman
+              yang komprehensif dan terperinci. Itulah sebabnya kami hadir,
+              sebagai sumber informasi hukum yang tepercaya dan terkini.
+            </p>
+          </div>
+          <div className='w-[288px]'>
+            <img src={feature3} alt='' />
+            <h1 className='text-[32px]'>Konsultasi Hukum</h1>
+            <p className='text-[16px]'>
+              Dalam dunia yang kompleks ini, pengetahuan adalah kekuatan. Kami
+              memahami bahwa setiap keputusan hukum Anda membutuhkan pemahaman
+              yang komprehensif dan terperinci. Itulah sebabnya kami hadir,
+              sebagai sumber informasi hukum yang tepercaya dan terkini.
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Bantuan Hukum Terpecaya */}
+      <div>
+        <div>
+          <h1>Bantuan Hukum Terpecaya</h1>
+        </div>
+        <div>
+          <div className='w-[320px]'>
+            <div>
+              <img src={profile1} alt='' />
+            </div>
+            <div className='flex items-center justify-between'>
+              <div>
+                <h1 className='text-xl'>Anne Maria</h1>
+                <p>Law Consultant</p>
+                <p>2+ Years Experience</p>
+              </div>
+              <div>
+                <h1>Pilih</h1>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
